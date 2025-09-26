d7139aa1d21e2deb3e75eb6f68b8c5126d280edb
natannascimento <natan.graves@gmail.com>
2025-09-26 16:13:24 -0300

    feat: aprimorar o design e a usabilidade dos componentes de personagem, incluindo melhorias no layout, estilos e funcionalidades

--- src/app/features/character/character-detail/character-detail.component.ts
+++ src/app/features/character/character-detail/character-detail.component.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { Character } from '../../../core/models/character.model';
 import { CharactersService } from '../../../core/services/character.service';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.scss']
 })
 export class CharacterDetailComponent {
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   private characterService = inject(CharactersService);
 
   character = signal<Character | null>(null);
@@ -30,9 +32,7 @@ export class CharacterDetailComponent {
     });
   }
 
-  private router = inject(Router);
-  
   goBack() {
-    this.router.navigate(['../']);
+    this.router.navigate(['/']);
   }
 }
